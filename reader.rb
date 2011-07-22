7a61a80bb4502cdae40fdee0060b378ece2bd84d
Brent Vatne <brentvatne@gmail.com>
2011-07-22 12:23:55 -0700

    Consolidated logic into one regexp, still need to get rid of leading meta data lines and also returns/newlines

--- reader.rb
+++ reader.rb
@@ -5,23 +5,11 @@ file = File.open("My Clippings.txt")
 contents = file.read
 
 separator = "="*10
-dictionary_name = "The New Oxford American Dictionary"
-dict_entry_regexp = /(?<=#{dictionary_name})(?x) (?# match dictionary name without consuming it, then turn on ignore whitespace mode)
-                     
-                     .*?                         (?# non-greedy match of any character until the next expression)
-                     (?=[=]{10})                 (?# stop matching at the separator, which is 10 "="'s by default)
-                    /m
+dictionary = "The New Oxford American Dictionary"
+entry_regexp = /(?<=#{dictionary})(?x) (?# match dictionary name without consuming it, then turn on ignore whitespace mode)
+                .*?                         (?# non-greedy match of any character until the next expression)
+                (?=[=]{10})                 (?# stop matching at the separator, which is 10 "="'s by default)
+               /m
 
-all_entries = contents.scan(dict_entry_regexp)
-                      #.join
-                      #.split(separator)
+all_entries = contents.scan(entry_regexp)
 pp all_entries[0]
-
-#contents.scan(dictionary_entry) do |line|
-#  puts line
-#end
-
-
-# 
-# .gsub(header_line_regexp,"")
-# .gsub(highlight_info_line_regexp,"")
