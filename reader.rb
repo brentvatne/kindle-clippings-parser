4a36d968d60c39dbc94b16ea4ec16282eb905145
Brent Vatne <brentvatne@gmail.com>
2011-07-22 20:12:58 -0700

    Remove junk meta data

--- reader.rb
+++ reader.rb
@@ -4,12 +4,21 @@ require 'pp'
 file = File.open("My Clippings.txt")
 contents = file.read
 
-separator = "="*10
-dictionary = "The New Oxford American Dictionary"
-entry_regexp = /(?<=#{dictionary})(?x) (?# match dictionary name without consuming it, then turn on ignore whitespace mode)
-                .*?                         (?# non-greedy match of any character until the next expression)
-                (?=[=]{10})                 (?# stop matching at the separator, which is 10 "="'s by default)
-               /m
+#clean out metadata that we don't want
+newlines_and_spaces = "[\s\\r\\n\r\n ]"
+junk_meta_regexp = /^.*?Highlight.*?(Added on)(?x)
+                    .*?(\d{2}:\d{2}\s(AM|PM))
+                    #{newlines_and_spaces}*
+                   /
+contents = contents.gsub(junk_meta_regexp,"")
 
+#extract dictionary entries               
+separator      = "="*10
+dictionary     = "The New Oxford American Dictionary"
+entry_regexp   = /(?<=#{dictionary})(?x) (?# match dictionary name without consuming it, then turn on ignore whitespace mode)
+                 .*?                     (?# non-greedy match of any character until the next expression)
+                 (?=#{separator})        (?# stop matching at the separator, which is 10 "="'s by default)
+                 /m
+                 
 all_entries = contents.scan(entry_regexp)
 pp all_entries[0]
