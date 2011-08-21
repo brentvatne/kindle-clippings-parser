7ebd1c71f93b45a162d119ff4babcad5e4a1c133
Brent Vatne <brentvatne@gmail.com>
2011-08-21 15:18:46 -0700

    Before pushing to github

--- reader.rb
+++ reader.rb
@@ -16,29 +16,29 @@ class KindleClippingsParser
   def delete_returns(str)
     str = str.gsub(/[\r]/,"")
   end
-
-  def parse_entries
-    #split by separator
-    separator   = "="*10
-    raw_entries = @contents.split(separator)
-    raw_entries.map! { |entry| trim_leading_newline(entry) }
-    parsed_entries = []
-    
-    #Author
-    #eg: "For Whom the Bell Tolls (Ernest Hemingway)"
+  
+  #eg: "For Whom the Bell Tolls (Ernest Hemingway)"
+  #Matches: "(Ernest Hemingway)"  
+  def parse_author(entry)
     parenthesis_regexp = /[\(\)]/ #matches ( or )
-    #Matches: "(Ernest Hemingway)"
     author_regexp =  /(\()        (?# opening parenthesis)
                       [^\)^\(]*?  (?# anything that is not a parenthesis - non-greedy)
                       (\))        (?# closing parenthesis)
                       \Z          (?# this must be at the end of the line because book title may contain parenthesis)
                      /x
     
-    #Date Added
-    #eg: "- Highlight Loc. 10177-78 | Added on Sunday, October 24, 2010, 04:32 PM"       
+    author = lines[0].match(author_regexp).to_a.first or ""
+    author.gsub!(parenthesis_regexp,"")              
+    
+    return author
+  end
+  
+  #eg: "- Highlight Loc. 10177-78 | Added on Sunday, October 24, 2010, 04:32 PM" 
+  #Matches: "Sunday, October 24, 2010, 04:32 PM" 
+  def parse_date(entry)
     days = %w{Monday Tuesday Wednesday Thursday Friday Saturday Sunday}.join("|")
     months = %w{January February March April May June July August September October November December}.join("|")
-    #Matches: "Sunday, October 24, 2010, 04:32 PM"
+    
     date_regexp = /(#{days}),\s           (?# "Monday, " days followed by comma and a space)
                    (#{months})\s\d{2},\s  (?# "January 05, " month followed by comma and a space)
                    \d{4},\s               (?# "2011, " year followed by comma and space)
@@ -46,51 +46,66 @@ class KindleClippingsParser
                    (AM|PM)                (?# "AM")
                    \Z                     (?# End of string)
                   /x
-     
-    #Entry Type and Position:
-    #eg: "- Highlight Loc. 10177-78 | Added on Sunday, October 24, 2010, 04:32 PM"            
-    #Matches: "Highlight"
+                  
+    date = lines[1].match(date_regexp).to_a.first or ""
+  end
+  
+  def parse_title
+    lines.first.gsub!(author_regexp,"") #clear out the author to simplify getting the title
+    title = lines.first.strip    
+  end
+  
+  #eg: "- Highlight Loc. 10177-78 | Added on Sunday, October 24, 2010, 04:32 PM"            
+  #Matches: "Highlight"
+  def parse_type
     entry_type_regexp = /(?<=-\s)         (?# look behind for "- " this marks the beginning of the line)
                          (Highlight|Note) (?# match either Highlight or Note, the two types of entries to clippings)
-                        /x
-    #Matches: "0177-78" 
+                        /x        
+    #highlight type
+    entry_type = lines[1].match(entry_type_regexp).to_a.first || ""
+  end
+  
+  #Matches: "0177-78" 
+  def parse_location(entries)
     highlight_position_regexp = /(?<=Loc\.\s) (?# look behind to match "Loc. " which appears before the position number)
                                  \d*          (?# match any number of digits - books can be any length)
                                  ([-]\d*)?    (?# may have a - followed by another number of arbitrary length)
                                  (?=\s|)      (?# lastly just to be safe double check that it is followed by a " | ")
                                 /x
+
+    highlight_location = lines[1].match(highlight_position_regexp).to_a.first || ""
+  end
+  
+  def parse_content(entries)
+    contents = lines[2..lines.length].join
+  end
+  
+
+  def parse_entry(entry)
+    lines = entry.split(/\n/)
     
+    unless lines.length == 0
+      author = parse_author(lines)
+      title = parse_title(lines)
+      type = parse_type(lines)
+      location = parse_location(lines)
+      contents = parse_content(lines)
+
+      { :title  => title, :author => author, :date => date, :type => entry_type, 
+        :location => highlight_location :contents => contents }  
+    end
+  end
+
+  
+  def parse
+    separator   = "="*10
+    raw_entries = @contents.split(separator)
+    raw_entries.map! { |entry| trim_leading_newline(entry) }
+    
+    parsed_entries = []
+
     raw_entries.each do |entry|
-      lines = entry.split(/\n/)
-      if lines.length > 0
-        #author
-        author = lines[0].match(author_regexp).to_a.first
-        author.gsub!(parenthesis_regexp,"") unless author.nil?
-        
-        #title
-        lines.first.gsub!(author_regexp,"") #clear out the author to simplify getting the title
-        title = lines.first.strip
-        
-        #highlight type
-        entry_type = lines[1].match(entry_type_regexp).to_a.first
-        
-        #highlight location
-        highlight_location = lines[1].match(highlight_position_regexp).to_a.first
-        
-        #added date
-        date = lines[1].match(date_regexp).to_a.first
-        
-        #contents
-        contents = lines[2..lines.length].join
-        
-        #push to entries
-        parsed_entries.push( { :title  => title,
-                               :author => author,
-                               :date => date,
-                               :type => entry_type,
-                               :location => highlight_location,
-                               :contents => contents } )
-      end
+      parsed_entries << parse_entry(entry)
     end
     
     parsed_entries
@@ -100,18 +115,7 @@ class KindleClippingsParser
     str.gsub(/\A\n/,"")
   end
   
-  def get_dictionary_entries(dictionary = "The New Oxford American Dictionary")
-  end
-  
-  def get_entries_by_book_name(book_name)
-    
-  end
-  
-  def get_entries_containing_string(str)
-    
-  end
-  
 end
 
-parser = KindleClippingsParser.new("My Clippings.txt")
-#puts parser.get_dictionary_entries()
+clippings_parsers = KindleClippingsParser.new("My Clippings.txt")
+clippings_parse.parse
